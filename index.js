5715355891a27d2ecefd9666e2648a5318f5557c
angel <angeldrummer2000@gmail.com>
2024-07-19 06:10:20 -0700

    project added

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/api/whoami', function(req,res){
+  let ipaddress = req.ip;
+  let language = req.headers['accept-language'];
+  let software = req.headers['user-agent'];
+  res.json({ "ipaddress": ipaddress, "language": language, "software": software});
+})
+
+
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
